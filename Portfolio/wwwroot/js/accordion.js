30b1cafa9141eed60ff756009e4522a87e3bf11b
Juan Diego Sánchez <jdsang18@gmail.com>
2025-10-20 17:49:44 -0600

    Changes in AboutMe, Navbar, Projects and the Index

---
+++ Portfolio/wwwroot/js/accordion.js
@@ -0,0 +1,7 @@
+﻿const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach(accordion => {
+    accordion.addEventListener("click", () => {
+        accordion.classList.toggle("active");
+    });
+});
