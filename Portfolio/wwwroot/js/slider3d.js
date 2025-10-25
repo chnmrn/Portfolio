ce1fb2d7c2d096bdcd4517857e1859f30b136ccb
Juan Diego Sánchez <jdsang18@gmail.com>
2025-10-24 23:01:04 -0600

    Implementation of redirection in the images

---
+++ Portfolio/wwwroot/js/slider3d.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".banner .slider .item"); // Select all items within the slider
+
+    items.forEach((item, index) => {
+        item.addEventListener("click", () => {    // Add click event listener to each item
+            const link = item.dataset.link;
+            if (link) {
+                window.location.href = link;  // Redirect to the URL specified in the project
+            }
+        });
+    });
+});
